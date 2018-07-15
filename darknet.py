4ab5959827519ed590ce29bfcb24ae88e0d28f36
Mayukh Sattiraju <msattir@clemson.edu>
2018-07-15 02:10:40 -0400

    module build completed

--- darknet.py
+++ darknet.py
@@ -30,7 +30,7 @@ def parse_cfg(cfg_file):
      else:
         key,value = line.split('=')
         block[key.rstrip()] = value.lstrip()
-  block.append(block)
+  blocks.append(block)
 
   return(blocks)  
 
@@ -68,12 +68,12 @@ def create_modules(blocks):
 
         #Add cnn layer
         conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, padding=pad, bias=bias)
-        module.add("conv_{0}".format(index), conv)
+        module.add_module("conv_{0}".format(index), conv)
 
         #Batch Norm Layer
         if batch_normalize:
            bn = nn.BatchNorm2d(filters)
-           module.add_module("batch_norm_{0}".formaat(index), bn)
+           module.add_module("batch_norm_{0}".format(index), bn)
         
         #Activation Layer
         if activation == "leaky":
@@ -87,5 +87,59 @@ def create_modules(blocks):
         upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
         module.add_module("upsample_{}".format(index), upsample)
 
-     
-     
+     #residual layer
+     elif(x["type"] == "route"):
+        x["layers"] = x["layers"].split(',')
+        #start a route
+        start = int(x["layers"][0])
+        #search for end, if exists
+        try:
+           end = int(x["layers"][1])
+        except:
+           end = 0
+        if start > 0:
+           start = start - index
+        if end > 0:
+           end = end - index
+        route = EmptyLayer()
+        module.add_module("route_{0}".format(index), route)
+        if end < 0:
+           filters = output_filters[index + start] + output_filters[index + end]
+        else:
+           filters = output_filters[index + start]
+       
+     #skip connections
+     elif(x["type"] == "shortcut"):
+        shortcut = EmptyLayer()
+        module.add_module("shortcut_{}".format(index), shortcut)
+
+     #Yolo detection layer
+     elif(x["type"] == "yolo"):
+        mask = (x["mask"]).split(',')
+        mask = [int(x) for x in mask]
+        
+        anchors = x["anchors"].split(',')
+        anchors = [int(x) for x in anchors]
+        anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+        anchors = [anchors[i] for i in mask]
+
+        detection = DetectionLayer(anchors)
+        module.add_module("Detection_{}".format(index), detection)
+
+     module_list.append(module)
+     prev_filters = filters
+     output_filters.append(filters)
+  
+  return(net_info, module_list)
+
+class EmptyLayer(nn.Module):
+  def __init__(self): 
+     super(EmptyLayer, self).__init__()
+
+class DetectionLayer(nn.Module):
+  def __init__(self, anchors):
+     super(DetectionLayer, self).__init__()
+     self.anchors = anchors
+        
+blocks = parse_cfg("cfg/yolov3.cfg")
+print(create_modules(blocks))     
